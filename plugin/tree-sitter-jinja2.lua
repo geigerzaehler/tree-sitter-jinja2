8565bc4b2148e5c96968f074053c36aff24572f9
HiPhish <hiphish@posteo.de>
2025-11-02 18:46:10 +0100

    Move Neovim plugin initialization to `plugin` file
    Saves to from calling the Lua function `require` on startup, which would
    search the entire runtime path for the Lua module.

--- plugin/tree-sitter-jinja2.lua
+++ plugin/tree-sitter-jinja2.lua
@@ -1,1 +1,52 @@
-require("tree-sitter-jinja2").init()
+local function register_parser()
+  local parsers = require("nvim-treesitter.parsers")
+
+  -- Register the plugin path as a tree-sitter parser to nvim-treesitter.
+  -- This is not required, but it allows nvim-treesitter to manage it e.g. :TS{Install,Update} jinja2
+  local file = debug.getinfo(1).source:match("@(.*/)")
+  local plugin_dir = vim.fn.fnamemodify(file, ":p:h:h")
+
+  --- @type ParserInfo[]
+  local parser_config
+
+  if type(parsers.get_parser_configs) == "function" then
+    parser_config = parsers.get_parser_configs()
+  else
+    -- "nvim-treesitter.parsers" exports the parser table itself starting nvim-treesitter v1.x
+    parser_config = parsers
+  end
+
+  parser_config.jinja2 = {
+    install_info = {
+      url = plugin_dir,
+      files = { "src/parser.c" },
+      branch = "main",
+    },
+    filetype = "jinja2",
+  }
+end
+
+local function init()
+  register_parser()
+
+  vim.treesitter.language.add("htmljinja", {
+    path = vim.api.nvim_get_runtime_file("parser/jinja2.so", false)[1],
+    symbol_name = "jinja2",
+  })
+
+  vim.filetype.add({
+    extension = {
+      j2 = "jinja2",
+      jinja = "jinja2",
+      jinja2 = "jinja2",
+    },
+    pattern = {
+      [".*/jinja2/.*%.html"] = "htmljinja",
+      [".*%.html%.j2"] = "htmljinja",
+      [".*%.html%.jinja"] = "htmljinja",
+      [".*%.html%.jinja2"] = "htmljinja",
+    },
+  })
+end
+
+init()
